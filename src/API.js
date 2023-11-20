47b4890af729fb57dd8938f3428e995ac3102e84
Japheth Mbuvi <japhethmbuvi002@gmail.com>
2023-11-20 07:35:05 +0300

    changes

--- src/API.js
+++ src/API.js
@@ -1,1 +1,19 @@
+// import { useEffect, useState } from "react";
+// import moment from "moment/moment";
+
 export const URL = "http://localhost/back-end/";
+
+// const created_on = () => {
+//   const [currentDate, setCurrentDate] = useState("");
+//   const [currentTime, setCurrentTime] = useState("");
+
+//   useEffect(() => {
+//     var date = moment().utcOffset("").format(" DD/MMM/YYYY");
+//     var time = moment().utcOffset("").format(" HH:mm");
+
+//     setCurrentDate(date);
+//     setCurrentTime(time);
+//   }, []);
+// };
+
+// // export const created_on = `${currentDate} ${currentTime}`;

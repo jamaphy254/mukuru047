c614d75b5f29db09039e11d82bcc9d288fd99d5b
Japheth Mbuvi <japhethmbuvi002@gmail.com>
2023-12-05 12:51:53 +0300

    Update NewPost.js

--- src/pages/NewPost.js
+++ src/pages/NewPost.js
@@ -27,7 +27,7 @@ const NewPost = () => {
 
   // Convert first letter to uppercase and rest to lowercase
   const text =
-    post_text.charAt(0).toUpperCase() + post_text.slice(1);
+    post_text.charAt(0).toUpperCase() + post_text.slice(1).toLowerCase();
 
   const url = `${URL}posts.php`;
 
